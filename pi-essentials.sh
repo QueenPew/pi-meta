a34f0690ca9e0c6720f86a54a0d88fbd8ae21a39
heavygabriel <leolegrosnono@gmail.com>
2021-01-29 15:29:58 -0500

    Made mesa work.

--- pi-essentials.sh
+++ pi-essentials.sh
@@ -6,7 +6,7 @@ sudo apt update;
 echo "Upgrading packages...";
 sudo apt upgrade -y ;
 echo "Installing packages...";
-sudo apt install build-essential cmake git ninja wget curl chromium-browser mesa-utils python3 gcc -y;
+sudo apt install build-essential cmake git wget curl chromium-browser mesa-utils python3 gcc llvm -y;
 echo "Getting mesa dependencies.";
 sudo apt-get install libxcb-randr0-dev libxrandr-dev  libxcb-xinerama0-dev libxinerama-dev libxcursor-dev libxcb-cursor-dev libxkbcommon-dev xutils-dev xutils-dev libpthread-stubs0-dev libpciaccess-dev libffi-dev x11proto-xext-dev libxcb1-dev libxcb-*dev bison flex libssl-dev libgnutls28-dev x11proto-dri2-dev x11proto-dri3-dev libx11-dev libxcb-glx0-dev libx11-xcb-dev libxext-dev libxdamage-dev libxfixes-dev libva-dev x11proto-randr-dev x11proto-present-dev libclc-dev libelf-dev git build-essential mesa-utils libvulkan-dev ninja-build libvulkan1 python-mako libdrm-dev libxshmfence-dev libxxf86vm-dev python3-mako python3-pip libzstd-dev pkg-config -y
 sudo pip3 install meson
@@ -19,7 +19,7 @@ git clone git://anongit.freedesktop.org/mesa/mesa
 cd mesa
 git fetch
 git checkout 20.3
-meson -Dplatforms=x11 -Dvulkan-drivers=broadcom -Ddri-drivers= -Dgallium-drivers=v3d,kmsro,vc4,virgl -Dbuildtype=release builddir/ # Compiles Mesa for the Pi 4(and below?).
+meson --prefix /usr -Dplatforms=x11 -Dvulkan-drivers=broadcom -Ddri-drivers= -Dgallium-drivers=v3d,kmsro,vc4,virgl -Dbuildtype=release builddir/ # Compiles Mesa for the Pi 4(and below?).
 ninja -C builddir/
 sudo ninja -C builddir/ install 
 echo "Done! Please reboot when convenient."
